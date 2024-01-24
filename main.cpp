3ed142a56736e0dfd6722534a98c8c23f6a3d7e8
samscan <scanlons@union.edu>
2024-01-24 22:41:15 +0000

    gooder

--- main.cpp
+++ main.cpp
@@ -1,171 +1,25 @@
-//=====[Libraries]=============================================================
-
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-//=====[Defines]===============================================================
-/*
-#define NUMBER_OF_KEYS                           4
-#define BLINKING_TIME_GAS_ALARM               1000
-#define BLINKING_TIME_OVER_TEMP_ALARM          500
-#define BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM  100
-#define POTENTIOMETER_OVER_TEMP_LEVEL            0.5
-#define TIME_INCREMENT_MS                       10
-
-//=====[Declaration and initialization of public global objects]===============
-
-DigitalIn enterButton(BUTTON1);
-DigitalIn gasDetector(D2);
-DigitalIn aButton(D4);
-DigitalIn bButton(D5);
-DigitalIn cButton(D6);
-DigitalIn dButton(D7);
-
-DigitalOut alarmLed(LED1);
-DigitalOut incorrectCodeLed(LED3);
-DigitalOut systemBlockedLed(LED2);
-*/
 UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
-AnalogIn potentiometer(A0);
+AnalogIn potentiometer(A2);
 
-//=====[Declaration and initialization of public global variables]=============
-/*
-bool alarmState    = OFF;
-bool gasDetectorState      = OFF;
-bool overTempDetectorState = OFF;
-
-bool incorrectCode = false;
-bool overTempDetector = OFF;
-
-int numberOfIncorrectCodes = 0;
-int buttonBeingCompared    = 0;
-int codeSequence[NUMBER_OF_KEYS]   = { 1, 1, 0, 0 };
-int buttonsPressed[NUMBER_OF_KEYS] = { 0, 0, 0, 0 };
-int accumulatedTimeAlarm = 0;
-*/
 float potentiometerReading = 0.0;
 float startingAngle = 0.0;
 
-
-//=====[Declarations (prototypes) of public functions]=========================
-/*
-void inputsInit();
-void outputsInit();
-
-void alarmActivationUpdate();
-void alarmDeactivationUpdate();
-*/
 void uartTask();
 float potentiometerAngle(float potentiometerReading);
 void setStartingAngle();
-/*
-void availableCommands();
-bool areEqual();
-*/
-//=====[Main function, the program entry point after power on or reset]========
 
 int main()
 {
-    /*
-    inputsInit();
-    outputsInit();
-    */
     setStartingAngle();
     while (true) {
-        //alarmActivationUpdate();
-        //alarmDeactivationUpdate();
         uartTask();
-       // delay(TIME_INCREMENT_MS);
     }
 }
 
-//=====[Implementations of public functions]===================================
-/*
-void inputsInit()
-{
-    gasDetector.mode(PullDown);
-    aButton.mode(PullDown);
-    bButton.mode(PullDown);
-    cButton.mode(PullDown);
-    dButton.mode(PullDown);
-}
-
-void outputsInit()
-{
-    alarmLed = OFF;
-    incorrectCodeLed = OFF;
-    systemBlockedLed = OFF;
-}
-
-void alarmActivationUpdate()
-{
-    potentiometerReading = potentiometer.read();
-
-    if ( potentiometerReading > POTENTIOMETER_OVER_TEMP_LEVEL ) {
-        overTempDetector = ON;
-    } else {
-        overTempDetector = OFF;
-    }
-
-    if( gasDetector) {
-        gasDetectorState = ON;
-        alarmState = ON;
-    }
-    if( overTempDetector ) {
-        overTempDetectorState = ON;
-        alarmState = ON;
-    }
-    if( alarmState ) { 
-        accumulatedTimeAlarm = accumulatedTimeAlarm + TIME_INCREMENT_MS;                      
-    
-        if( gasDetectorState && overTempDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_AND_OVER_TEMP_ALARM ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        } else if( gasDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_GAS_ALARM ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        } else if ( overTempDetectorState ) {
-            if( accumulatedTimeAlarm >= BLINKING_TIME_OVER_TEMP_ALARM  ) {
-                accumulatedTimeAlarm = 0;
-                alarmLed = !alarmLed;
-            }
-        }
-    } else{
-        alarmLed = OFF;
-        gasDetectorState = OFF;
-        overTempDetectorState = OFF;
-    }
-}
-
-void alarmDeactivationUpdate()
-{
-    if ( numberOfIncorrectCodes < 5 ) {
-        if ( aButton && bButton && cButton && dButton && !enterButton ) {
-            incorrectCodeLed = OFF;
-        }
-        if ( enterButton && !incorrectCodeLed && alarmState ) {
-            buttonsPressed[0] = aButton;
-            buttonsPressed[1] = bButton;
-            buttonsPressed[2] = cButton;
-            buttonsPressed[3] = dButton;
-            if ( areEqual() ) {
-                alarmState = OFF;
-                numberOfIncorrectCodes = 0;
-            } else {
-                incorrectCodeLed = ON;
-                numberOfIncorrectCodes++;
-            }
-        }
-    } else {
-        systemBlockedLed = ON;
-    }
-}
-*/
 void uartTask()
 {
     char receivedChar = '\0';
@@ -174,104 +28,6 @@ void uartTask()
     if( uartUsb.readable() ) {
         uartUsb.read( &receivedChar, 1 );
         switch (receivedChar) {
-        /*
-        case '1':
-            if ( alarmState ) {
-                uartUsb.write( "The alarm is activated\r\n", 24);
-            } else {
-                uartUsb.write( "The alarm is not activated\r\n", 28);
-            }
-            break;
-
-        case '2':
-            if ( gasDetector ) {
-                uartUsb.write( "Gas is being detected\r\n", 22);
-            } else {
-                uartUsb.write( "Gas is not being detected\r\n", 27);
-            }
-            break;
-
-        case '3':
-            if ( overTempDetector ) {
-                uartUsb.write( "Temperature is above the maximum level\r\n", 40);
-            } else {
-                uartUsb.write( "Temperature is below the maximum level\r\n", 40);
-            }
-            break;
-            
-        case '4':
-            uartUsb.write( "Please enter the code sequence.\r\n", 33 );
-            uartUsb.write( "First enter 'A', then 'B', then 'C', and ", 41 ); 
-            uartUsb.write( "finally 'D' button\r\n", 20 );
-            uartUsb.write( "In each case type 1 for pressed or 0 for ", 41 );
-            uartUsb.write( "not pressed\r\n", 13 );
-            uartUsb.write( "For example, for 'A' = pressed, ", 32 );
-            uartUsb.write( "'B' = pressed, 'C' = not pressed, ", 34);
-            uartUsb.write( "'D' = not pressed, enter '1', then '1', ", 40 );
-            uartUsb.write( "then '0', and finally '0'\r\n\r\n", 29 );
-
-            incorrectCode = false;
-
-            for ( buttonBeingCompared = 0; 
-                  buttonBeingCompared < NUMBER_OF_KEYS; 
-                  buttonBeingCompared++) {
-
-                uartUsb.read( &receivedChar, 1 );
-                uartUsb.write( "*", 1 );
-
-                if ( receivedChar == '1' ) {
-                    if ( codeSequence[buttonBeingCompared] != 1 ) {
-                        incorrectCode = true;
-                    }
-                } else if ( receivedChar == '0' ) {
-                    if ( codeSequence[buttonBeingCompared] != 0 ) {
-                        incorrectCode = true;
-                    }
-                } else {
-                    incorrectCode = true;
-                }
-            }
-
-            if ( incorrectCode == false ) {
-                uartUsb.write( "\r\nThe code is correct\r\n\r\n", 25 );
-                alarmState = OFF;
-                incorrectCodeLed = OFF;
-                numberOfIncorrectCodes = 0;
-            } else {
-                uartUsb.write( "\r\nThe code is incorrect\r\n\r\n", 27 );
-                incorrectCodeLed = ON;
-                numberOfIncorrectCodes++;
-            }                
-            break;
-
-        case '5':
-            uartUsb.write( "Please enter new code sequence\r\n", 32 );
-            uartUsb.write( "First enter 'A', then 'B', then 'C', and ", 41 );
-            uartUsb.write( "finally 'D' button\r\n", 20 );
-            uartUsb.write( "In each case type 1 for pressed or 0 for not ", 45 );
-            uartUsb.write( "pressed\r\n", 9 );
-            uartUsb.write( "For example, for 'A' = pressed, 'B' = pressed,", 46 );
-            uartUsb.write( " 'C' = not pressed,", 19 );
-            uartUsb.write( "'D' = not pressed, enter '1', then '1', ", 40 );
-            uartUsb.write( "then '0', and finally '0'\r\n\r\n", 29 );
-
-            for ( buttonBeingCompared = 0; 
-                  buttonBeingCompared < NUMBER_OF_KEYS; 
-                  buttonBeingCompared++) {
-
-                uartUsb.read( &receivedChar, 1 );
-                uartUsb.write( "*", 1 );
-
-                if ( receivedChar == '1' ) {
-                    codeSequence[buttonBeingCompared] = 1;
-                } else if ( receivedChar == '0' ) {
-                    codeSequence[buttonBeingCompared] = 0;
-                }
-            }
-
-            uartUsb.write( "\r\nNew code generated\r\n\r\n", 24 );
-            break;
- */
         case 'p':
         case 'P':
             potentiometerReading = potentiometer.read();
@@ -287,38 +43,9 @@ void uartTask()
             stringLength = strlen(str);
             uartUsb.write( str, stringLength );
             break;
-       // default:
-            //availableCommands();
-          //  break;
-
         }
     }
 }
-/*
-void availableCommands()
-{
-    uartUsb.write( "Available commands:\r\n", 21 );
-    uartUsb.write( "Press '1' to get the alarm state\r\n", 34 );
-    uartUsb.write( "Press '2' to get the gas detector state\r\n", 41 );
-    uartUsb.write( "Press '3' to get the over temperature detector state\r\n", 54 );
-    uartUsb.write( "Press '4' to enter the code sequence\r\n", 38 );
-    uartUsb.write( "Press '5' to enter a new code\r\n", 31 );
-    uartUsb.write( "Press 'P' or 'p' to get potentiometer reading\r\n\r\n", 49 );
-}
-
-bool areEqual()
-{
-    int i;
-
-    for (i = 0; i < NUMBER_OF_KEYS; i++) {
-        if (codeSequence[i] != buttonsPressed[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-*/
 float potentiometerAngle(float potentiometerReading) {
     return (potentiometerReading * 10 * 360 - startingAngle);
 }
